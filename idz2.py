f7e3a709c4863a193829c89f802e3d8c58c25d0d
Timofej <tortik8971@bk.ru>
2021-05-05 22:18:19 +0300

    Обновлнены задания

--- idz2.py
+++ idz2.py
@@ -1,8 +1,13 @@
+'''
+Дано слово. Поменять местами его третью и последнюю буквы.
+'''
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-slovo = input("Введите слово: ")
-x = [slovo[-1], slovo[2],]
-y = slovo[0:2]+ x[0]+slovo[3:-1]+x[1]
-print (y)
+if __name__ == '__main__':
+    slovo = input("Введите слово: ")
 
+    x = [slovo[-1], slovo[2],]
+    y = slovo[0:2] + x[0] + slovo[3:-1] + x[1]
+
+    print (y)
--- idz3.py
+++ idz3.py
@@ -1,16 +1,20 @@
+'''
+Дан текст. Найти наибольшее количество идущих подряд цифр.
+'''
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-text = input("Введите текст, содержащий символы: ")
-i = 0
-otvet = 0
+if __name__ == '__main__':
+    text = input("Введите текст, содержащий символы: ")
 
-for simvol in " "+text+" ":
-    if (simvol.isdigit()):
-        i += 1
-    else:
-        if otvet < i:
-            otvet = i
-        i = 0
+    i = 0
+    otvet = 0
+    for simvol in " "+text+" ":
+        if (simvol.isdigit()):
+            i += 1
+        else:
+            if otvet < i:
+                otvet = i
+            i = 0
 
-print(otvet)
+    print(otvet)
